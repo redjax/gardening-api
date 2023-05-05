32f464921258d7ef201f0b1d5fdcb0bdb51eacb4
redjax <none@none.com>
2023-05-05 02:21:09 -0400

    Add initial code

---
+++ trefle_plant_api/main.py
@@ -0,0 +1,17 @@
+from __future__ import annotations
+
+from core.config import app_settings, logging_settings
+from core.logging.logger import get_logger
+
+from util.time_utils import benchmark
+
+log = get_logger(__name__, level=logging_settings.LOG_LEVEL)
+print(f"Settings: {app_settings}")
+
+from util.validators import valid_req_libs
+
+count = 1000
+
+
+if __name__ == "__main__":
+    ...
