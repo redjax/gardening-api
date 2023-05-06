35bde001b19ce5b21965d8e01a2331a6f7dbd5c8
Jack <redjax@users.noreply.github.com>
2023-05-06 00:50:38 -0400

    Repo/scripts (#55) (#60)
    * Script: Prune git branches
    
    Add git branch prune script
    
    * Add async requests:
    
    Add code to request individual endpoints asynchronously.
    
    Add code to request multiple plants from a list asyncronously.
    
    Add benchmarks
    
    ---------
    
    Co-authored-by: redjax <none@none.com>

--- trefle_plant_api/util/constants.py
+++ trefle_plant_api/util/constants.py
@@ -2,9 +2,16 @@ from __future__ import annotations
 
 from typing import Union
 
-from core.config import api_settings, logging_settings
+from core.config import trefle_api_settings, logging_settings
 from core.logging.logger import get_logger
 
 log = get_logger(__name__, level=logging_settings.LOG_LEVEL)
 
 default_req_cache_dir = ".cache"
+
+trefle_base_url = trefle_api_settings.BASE_URL
+token_str = trefle_api_settings.API_KEY
+
+all_genus_endpoint = "/genus"
+all_plants_endpoint = "/plants"
+all_species_endpoint = "/species"
